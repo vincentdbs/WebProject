c44e076efbc3c5b49e6413d00e21c70898a3a257
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2019-10-15 05:13:41 +0200

    patch affichage register connexion

--- JS/loginSwitch.js
+++ JS/loginSwitch.js
@@ -1,37 +1,66 @@
+let register = document.getElementById("register");
+let login = document.getElementById("login");
 switchConnexion();
 
 function switchConnexion(){
-    let left = document.getElementById("register");
-    let right = document.getElementById("login");
+    let leftLogin = document.getElementById("left-half-login");
+    let rightLogin = document.getElementById("right-half-login");
+    register.style.display = "none";
+    login.style.display = "block";
+    if (window.innerWidth >= 480){
+        leftLogin.style.backgroundImage = "linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(\"../Image/porsche_face.jpg\")";
+        leftLogin.style.backgroundSize = "cover";
+        leftLogin.style.display = "block";
+        rightLogin.style.backgroundImage = "none";
+        rightLogin.style.backgroundColor = "white";
+        rightLogin.style.width = "50%";
 
-    left.style.display = "none";
-    right.style.display = "block";
-
-    let leftRegister = document.getElementById("right-half-login");
-    let rightRegister = document.getElementById("left-half-login");
-
-    leftRegister.style.backgroundImage = "linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(\"../Image/porsche_face.jpg\")";
-    leftRegister.style.backgroundSize = "cover";
-    rightRegister.style.backgroundImage = "none";
-    rightRegister.style.backgroundColor = "white";
-
-    console.log("connexion");
+        console.log("connexion sup 480");
+    }
+    else{
+        leftLogin.style.display = "none";
+        rightLogin.style.backgroundColor = "white";
+        rightLogin.style.width = "100%";
+        rightLogin.style.backgroundImage = "linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(\"../Image/porsche_face.jpg\")";
+        rightLogin.style.backgroundSize = "cover";
+        console.log("connexion inf 480");
+    }
 }
 
 function switchRegister() {
-    let register = document.getElementById("register");
-    let login = document.getElementById("login");
+    let leftRegister = document.getElementById("left-half-register");
+    let rightRegister = document.getElementById("right-half-register");
     register.style.display = "block";
     login.style.display = "none";
 
-    let leftRegister = document.getElementById("right-half-register");
-    let rightRegister = document.getElementById("left-half-register");
+    if (window.innerWidth >= 480){
+        rightRegister.style.backgroundImage = "linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(\"../Image/porsche_face.jpg\")";
+        rightRegister.style.backgroundSize = "cover";
+        rightRegister.style.display = "block";
+        leftRegister.style.display = "block";
+        leftRegister.style.backgroundImage = "none";
+        leftRegister.style.backgroundColor = "white";
+        leftRegister.style.width = "50%";
+        console.log("register sup 480");
+    }
+    else{
+        rightRegister.style.display = "none";
+        leftRegister.style.backgroundColor = "white";
+        leftRegister.style.width = "100%";
+        leftRegister.style.backgroundImage = "linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(\"../Image/porsche_face.jpg\")";
+        leftRegister.style.backgroundSize = "cover";
+        console.log("register inf 480");
+    }
+}
 
-    leftRegister.style.backgroundImage = "linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(\"../Image/porsche_face.jpg\")";
-    leftRegister.style.backgroundSize = "cover";
-    rightRegister.style.backgroundImage = "none";
-    rightRegister.style.backgroundColor = "white";
+window.addEventListener("resize", getWindowSize);
 
-
-    console.log("hfd")
+function getWindowSize(){
+    if(register.style.display === "none"){
+        switchConnexion()
+    }
+    else if (login.style.display === "none"){
+        console.log("register");
+        switchRegister()
+    }
 }
