cc8b5e0cac1f12a08a8fb50250f8870e8c847620
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2019-11-10 10:12:15 +0100

    add car dans database

---
+++ HTML/AddCar.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Add car</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="stylesheet" href="../CSS/Header.css">
+    <link rel="stylesheet" href="../CSS/All.css">
+    <link rel="stylesheet" href="../CSS/AddCar.css">
+    <link rel="stylesheet" href="../CSS/Text.css">
+    <link rel="stylesheet" href="../CSS/Button.css">
+</head>
+<body>
+    <?php
+    include "../HTML/Navbar.html";
+    ?>
+
+<main>
+    <div class="main-hero-image image">
+        <div class="center">
+            <form action="../PHP/InsertCar.php" method="post" class="form-add-car">
+                <h2>Add a car</h2>
+
+                <div class="div-input-label">
+                    <label class="labelToTop">Car name:</label>
+                    <input type="text" name="car_name" required>
+                </div>
+
+                <label>Brand:</label>
+                <input type="text" name="car_brand">
+                <label>Number of doors:</label>
+                <input type="text" name="car_nb_door">
+                <label>Number of seats:</label>
+                <input type="text" name="car_nb_seat">
+                <label>Price: </label>
+                <input type="text" name="car_price">
+                <label>Stock available: </label>
+                <input type="text" name="car_stock">
+                <select name="car_type">
+                    <option>Select a type</option>
+                    <option>Car</option>
+                    <option>Special</option>
+                    <option>Truck</option>
+                </select>
+                <button type="submit" class="button-booking">Add</button>
+            </form>
+        </div>
+    </div>
+</main>
+
+</body>
+</html>
---
+++ PHP/InsertCar.php
@@ -0,0 +1,23 @@
+<?php
+    include "Db_connexion.php";
+
+    $car_name = $_POST['car_name'];
+    $car_brand = $_POST['car_brand'];
+    $car_nb_door = $_POST['car_nb_door'];
+    $car_nb_seats= $_POST['car_nb_seat'];
+    $car_price = $_POST['car_price'];
+    $car_stock = $_POST['car_stock'];
+    $car_type = $_POST['car_type'];
+
+    echo "<script>" . $car_price . "</script>";
+
+    $sql = "INSERT INTO car(car_name, car_nb_doors, car_nb_seats, car_stocks, car_price, car_brand, car_type)
+            VALUES
+            ('$car_name', '$car_nb_door', '$car_nb_seats', '$car_stock', '$car_price', '$car_brand', '$car_type')";
+
+    if (!mysqli_query($con, $sql)) {
+        die("Error : " . mysqli_error($con));
+    }
+
+    mysqli_close($con);
+?>
