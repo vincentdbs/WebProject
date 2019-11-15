6ab4eb783a146b3cb97b1220f51fda30d0bda7a7
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2019-11-15 09:46:19 +0100

    patch login

--- PHP/Login.php
+++ PHP/Login.php
@@ -5,37 +5,30 @@
     $user_email = mysqli_real_escape_string($con, $_POST['email']);
     $password = mysqli_real_escape_string($con, $_POST['password']);
 
-    $sql = "Select * from user where user_email = '".$user_email."' and user_password = '".$password."'";
+    $sql = "Select * from user where user_email = '".$user_email."' && user_password = '".$password."'";
+
     $result = mysqli_query($con, $sql);
 
-    if(mysqli_num_rows($result)<=0)
-    {
-        $sql = "Select * from users where user_email = '".$user_email."' and user_password = '".$password."'";
-        $result = mysqli_query($con, $sql);
-        if(mysqli_num_rows($result)<=0)
-        {
-            echo "<script>alert('Wrong username / password !Please Try Again!');";
-            die("window.history.go(-1);</script>");
+    if(mysqli_num_rows($result) <=0 ){
+        die("<script>window.history.go(-1);</script>");
+    }
+    else{
+        while ($row = mysqli_fetch_array($result)){
+            $_SESSION['user_id'] = $row['user_id'];
+            $_SESSION['user_first_name'] = $row['user_first_name'];//use session
+            $_SESSION['user_last_name'] = $row['user_last_name'];//use session
+            $_SESSION['user_email'] = $row['user_email'];//use session
+            $_SESSION['user_role'] = $row['user_role'];
+
         }
-    }
-    if($row=mysqli_fetch_array($result))
-    {
-        $_SESSION['user_id'] = $row['user_id'];
-        $_SESSION['user_first_name'] = $row['user_first_name'];//use session
-        $_SESSION['user_last_name'] = $row['user_last_name'];//use session
-        $_SESSION['user_email'] = $row['user_email'];//use session
-        $_SESSION['user_role'] = $row['user_role'];
-    }
-    if($_SESSION['user_role']==="admin")
-    {
-        $name = $_SESSION['user_first_name'] . " " . $_SESSION['user_last_name'];
-        //echo "<script>window.location.href='../HTML/MyProfile.php';</script>";
-        header("location : MyProfile.php");
-    }
-    else if($_SESSION['user_role']==="user")
-    {
-        echo "window.location.href='../HTML/HomePage.php';</script>";
-        header("location : HomePage.php");
+
+        if($_SESSION['user_role']==="admin") {
+            header("location: ../HTML/MyProfile.php");
+        }
+        else if($_SESSION['user_role']==="user"){
+            echo "window.location.href='../HTML/HomePage.php';</script>";
+            header("location: ../HTML/HomePage.php");
+        }
     }
 ?>
 
