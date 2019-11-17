75ba4e55e73afb3d6f4cc358f9b8f21a75c4c679
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2019-11-17 08:24:52 +0100

    suppression booking avec la car supprimée

--- PHP/CarManagementDelete.php
+++ PHP/CarManagementDelete.php
@@ -3,5 +3,7 @@
     if(isset($_GET['car_id'])){
         $sql = "DELETE FROM car WHERE car_id=". $_GET['car_id'];
         mysqli_query($con, $sql);
+        $sql = "DELETE FROM booking WHERE booking_car_id=". $_GET['car_id'];
+        mysqli_query($con, $sql);
     }
 ?>
